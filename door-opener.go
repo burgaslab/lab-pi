a83e3e5eeb971e3c1fca4a70454717e78a05f665
Krasi Georgiev <krasi.root@gmail.com>
2017-04-21 13:27:23 +0300

    implemented pin control cli now supports setting pin number to control and toggle delay

--- door-opener.go
+++ door-opener.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -15,7 +16,16 @@ import (
 
 var gpioPins = []int{}
 
+const sysfs string = "/sys/class/gpio/"
+const sysfsGPIOenable string = sysfs + "export"
+const sysfsGPIOdisable string = sysfs + "unexport"
+
 var hanldeSignals = []os.Signal{syscall.SIGINT, syscall.SIGKILL}
+
+var config struct {
+	pin   string
+	delay time.Duration
+}
 
 func main() {
 
@@ -26,23 +36,54 @@ func main() {
 	app.Usage = "Raspberry Pi based golang app to control a door relay via a web interface"
 
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		cli.UintFlag{
 			Name:  "p,port",
-			Value: "8008",
+			Value: 80,
 			Usage: "port for the webserver",
+		},
+		cli.UintFlag{
+			Name:  "pp,pin",
+			Value: 21,
+			Usage: "GPIO pin to control",
+		},
+		cli.UintFlag{
+			Name:  "d,delay",
+			Value: 3,
+			Usage: "the total time in seconds that this GPIO pin will be set to high level",
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
+		// TODO validate the input using parse functions
+		config.pin = c.String("pin")
+		config.delay = time.Duration(c.Int("delay")) * time.Second
 
 		// start the signal handler as soon as we can to make sure that
 		// we don't miss any signals during boot
-		signals := make(chan os.Signal, 2048)
+		signals := make(chan os.Signal, 128)
 		signal.Notify(signals, hanldeSignals...)
 
+		// enable if not already enabled
+		if _, err := os.Stat(sysfs + "gpio" + config.pin); os.IsNotExist(err) {
+			// enable the GPIO interface and set gpion pin as an output
+			if _, err := os.Stat(sysfsGPIOenable); os.IsNotExist(err) {
+				log.Fatal(err)
+			}
+			if err := ioutil.WriteFile(sysfsGPIOenable, []byte(config.pin), 0644); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if err := ioutil.WriteFile(sysfs+"gpio"+config.pin+"/direction", []byte("out"), 0644); err != nil {
+			log.Fatal(err)
+		}
+
 		srv := &http.Server{Addr: ":" + c.String("port")}
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			io.WriteString(w, "hello world\n")
+
+		http.HandleFunc("/toggle", toggle)
+
+		http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+			io.WriteString(w, "status ok")
 		})
 		go func() {
 			log.Print("started web server on port :", c.String("port"))
@@ -62,6 +103,22 @@ func main() {
 
 }
 
+func toggle(w http.ResponseWriter, r *http.Request) {
+	go func() {
+		// toggle the pin output using a set delay
+		if err := ioutil.WriteFile(sysfs+"gpio"+config.pin+"/value", []byte("1"), 0644); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Pin %v enabled for %v seconds", config.pin, config.delay)
+		time.Sleep(config.delay)
+		if err := ioutil.WriteFile(sysfs+"gpio"+config.pin+"/value", []byte("0"), 0644); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Delay expired : pin %v set to disabled ", config.pin)
+	}()
+
+}
+
 func cleanup(signals chan os.Signal, srv *http.Server) error {
 	for s := range signals {
 		log.Print("Received signal: ", s)
@@ -71,11 +128,34 @@ func cleanup(signals chan os.Signal, srv *http.Server) error {
 			if err := srv.Shutdown(nil); err != nil {
 				panic(err) // failure/timeout shutting down the server gracefully
 			}
-			// []]byte("hello\ngo\n")
-			// echo 21 > /sys/class/gpio/unexport
+			err := ioutil.WriteFile(sysfsGPIOdisable, []byte(config.pin), 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
 			log.Print("cleaning up the mess I created")
 		}
 		return nil
 	}
 	return nil
 }
+
+// ParseLevel takes a string level and returns the Logrus log level constant.
+// func ParseDelay(lvl string) (Level, error) {
+// 	switch strings.ToLower(lvl) {
+// 	case "panic":
+// 		return PanicLevel, nil
+// 	case "fatal":
+// 		return FatalLevel, nil
+// 	case "error":
+// 		return ErrorLevel, nil
+// 	case "warn", "warning":
+// 		return WarnLevel, nil
+// 	case "info":
+// 		return InfoLevel, nil
+// 	case "debug":
+// 		return DebugLevel, nil
+// 	}
+//
+// 	var l Level
+// 	return l, fmt.Errorf("not a valid logrus Level: %q", lvl)
+// }
